bdd4573080ee66db9516fd51681e5c0098329d57
f100cleveland <dustin.rinne@gmail.com>
2016-05-01 22:19:07 -0500

    Change inline kernel building

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,8 +38,6 @@ GRAPHITE_OPTS := false
 USE_PIPE := true
 
 # Inline kernel building
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_GCC_VERSION_ARM64 := 7.0-kernel
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
 TARGET_KERNEL_CONFIG := saber_defconfig
